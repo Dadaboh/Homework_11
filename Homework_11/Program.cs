c58eb0535cc31f22bbc5df2ada7f1108c38bf9c9
Богдан Чиренко <bohdanchyrenko@gmail.com>
2023-10-15 00:23:13 +0300

    Добавьте файлы проекта.

---
+++ Homework_11/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Homework_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var o = new MyClass();
+
+            ASD(o.GetType());
+
+
+        }
+
+
+        public static void ASD(Type type)
+        {
+
+        }
+    }
+
+
+
+    public class MyClass
+    {
+        public int age;
+        public string name;
+
+        public bool isAlive { get; set; }
+
+        public MyClass()
+        {
+
+        }
+        public MyClass(int age, string name)
+        {
+            this.age = age;
+            this.name = name;
+        }
+
+    }
+}
